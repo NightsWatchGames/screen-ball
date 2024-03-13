49f4a0d13cb3c76f6396fa4d89e1b2657df349de
Linwei Zhang <lewiszlw520@gmail.com>
2024-03-13 22:20:19 +0800

    Update wall

--- src/camera.rs
+++ src/camera.rs
@@ -20,10 +20,8 @@ pub fn setup_camera(mut commands: Commands) {
     });
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, camera::CAMERA_HEIGHT_SIZE, 0.0)
+        transform: Transform::from_xyz(0.0, CAMERA_HEIGHT_SIZE, 0.0)
             .looking_at(Vec3::ZERO, Vec3::NEG_Z),
-        // transform: Transform::from_xyz(0.0, 50.0, 100.0).looking_at(Vec3::ZERO, Vec3::Y),
-        // transform: Transform::from_xyz(0.0, 0.0, 100.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
